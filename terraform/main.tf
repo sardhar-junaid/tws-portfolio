302c6278441de43630bf790575cc3936ef030aef
Ubuntu <azureuser@gitlearningvm.5dmly4d30r1elnmxeahmrxjpvd.bx.internal.cloudapp.net>
2025-07-10 11:20:43 +0000

    changes in index

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "portfolio_rg" {
+  name     = "tws-portfolio-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "portfolio_sa" {
+  name                     = "junaidportfoliosa123"
+  resource_group_name      = azurerm_resource_group.portfolio_rg.name
+  location                 = azurerm_resource_group.portfolio_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "index.html"
+  }
+}
+
---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "website_url" {
+  value = azurerm_storage_account.portfolio_sa.primary_web_endpoint
+}
+
